9e807503dd1443b359a265970615f4ec1746b470
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-27 18:09:34 +0300

    solution strings lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser.slice(0,1);
+
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+console.log (currentUser.length);
